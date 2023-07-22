0e796f7040723276950ab28073bb934e5d061ac2
Chua Jing Yong <jyongcode@gmail.com>
2023-07-23 01:43:04 +0800

    added new files

--- install-homescreen-shortcut.js
+++ install-homescreen-shortcut.js
@@ -1,24 +1,28 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
+  // Stash the event so it can be triggered later
   deferredPrompt = e;
-  // Show the "Install App" button when the app is eligible for installation
-  document.getElementById('installButton').style.display = 'block';
+  // Update UI notify the user they can add to home screen
+  let installButton = document.getElementById('installAppButton');
+  installButton.style.display = 'block';
 });
 
-document.getElementById('installButton').addEventListener('click', (e) => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-      // Hide the "Install App" button after user interaction
-      document.getElementById('installButton').style.display = 'none';
-    });
-  }
+document.getElementById('installAppButton').addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  let installButton = document.getElementById('installAppButton');
+  installButton.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
 });
